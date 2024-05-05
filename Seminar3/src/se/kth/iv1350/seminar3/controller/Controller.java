e83e1a224ed8df3bf8a20dd418fe7542bfb2fda0
suhaib921 <suheyb921@gmail.com>
2024-05-05 20:48:32 +0200

    Suhaib Abdi Muhummed

--- Seminar3/src/se/kth/iv1350/seminar3/controller/Controller.java
+++ Seminar3/src/se/kth/iv1350/seminar3/controller/Controller.java
@@ -11,7 +11,10 @@ import se.kth.iv1350.seminar3.integration.Printer;
 import se.kth.iv1350.seminar3.integration.DiscountRegister;
 import se.kth.iv1350.seminar3.modell.SaleLog;
 
-
+/**
+ * The Controller class acts as the mediator between the view and the model.
+ * It handles user actions, fetches data from the model, and decides the view to respond with.
+ */
 public class Controller {
 
     private AccountingSystem accSys;
@@ -22,53 +25,61 @@ public class Controller {
     private Sale sale;
     private SaleLog saleLog;
 
-
+    // Data transfer objects
     private SaleDTO saleDTO;
     private ItemDTO itemDTO;
 
     private double currentTotalPrice;
 
-    
-    public  Controller(){
-        accSys=new AccountingSystem();
+    /**
+     * Initializes a new instance of Controller.
+     * It sets up all system components required to process sales.
+     */
+
+    public Controller() {
+        accSys = new AccountingSystem();
         discount = new DiscountRegister();
         invSys = new InventorySystem();
         printer = new Printer();
         saleLog = new SaleLog();
-
     }
 
-    public void startSale(){
+    /**
+     * Starts a new sale transaction by initializing a Sale object.
+     */
+    public void startSale() {
         sale = new Sale();
     }
     
-
-    public SaleDTO scanItem(ItemDTO itemDTO, int quantity){
-
-        if(sale.findItemInfo(itemDTO)){
-
-            saleDTO = new SaleDTO (sale);
+    /**
+     * Adds an item to the current sale and updates the sale information.
+     *
+     * @param itemDTO the item to add
+     * @param quantity the quantity of the item
+     * @return updated sale information as a SaleDTO
+     */
+    public SaleDTO scanItem(ItemDTO itemDTO, int quantity) {
+        if (sale.findItemInfo(itemDTO)) {
+            saleDTO = new SaleDTO(sale);
         }
-
         return saleDTO;
-
     }
 
-     public void endSale(){
-       // sale.getCurrentTotalPrice();
-        scanItem(null);
-
+    /**
+     * Ends the current sale process.
+     * @return The calculated total price including tax.
+     */
+    public void endSale() {
+        return scanItem(null); 
     }
 
-
-    public void pay(double amountPaid){
-        
-
-        currentTotalPrice= sale.getCurrentTotalPrice();
-        Payment payment= new Payment(currentTotalPrice, amountPaid);
-
-    }
-
-      
+    /**
+     * Processes payment for the current sale.
+     *
+     * @param amountPaid the amount paid by the customer
+     */
+    public void pay(double amountPaid) {
+        currentTotalPrice = sale.getCurrentTotalPrice();
+        Payment payment = new Payment(currentTotalPrice, amountPaid);
+    }    
 }
-
--- Seminar3/src/se/kth/iv1350/seminar3/modell/Payment.java
+++ Seminar3/src/se/kth/iv1350/seminar3/modell/Payment.java
@@ -2,24 +2,44 @@ package se.kth.iv1350.seminar3.modell;
 
 import se.kth.iv1350.seminar3.dto.SaleDTO;
 
+/**
+ * Represents a payment transaction.
+ * This class handles the calculation of change due to the customer after a sale.
+ */
 public class Payment {
     private double currentTotalPrice;
     private double amountPaid;
 
-    public Payment(double currentTotalPrice, double amountPaid){
+    /**
+     * Constructs a Payment instance.
+     *
+     * @param currentTotalPrice the total price of the current sale
+     * @param amountPaid the amount paid by the customer
+     */
+    public Payment(double currentTotalPrice, double amountPaid) {
         this.currentTotalPrice = currentTotalPrice;
         this.amountPaid = amountPaid;
     }
 
+    /**
+     * Calculates the change due to the customer based on the amount paid and total price.
+     * This method is private because it is an internal utility function.
+     *
+     * @param amountPaid the amount paid by the customer
+     * @param currentTotalPrice the total price of the sale
+     * @return the calculated change to be given to the customer
+     */
     private double calculateCustomerChange(double amountPaid, double currentTotalPrice) {
-        double customerChange =  amountPaid - currentTotalPrice;
-        return customerChange;
+        return amountPaid - currentTotalPrice;
     }
 
-
-    public double getCustomerChange(){
+    /**
+     * Public method to get the calculated change for the customer.
+     * This leverages the private method to encapsulate the calculation logic.
+     *
+     * @return the change that should be given back to the customer
+     */
+    public double getCustomerChange() {
         return calculateCustomerChange(amountPaid, currentTotalPrice);
-    }  
-    
-
+    }    
 }
